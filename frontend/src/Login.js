4d0cf1cd3d530be183defe7f092551b762508923
Nishant Singh Ujjwal <nishantujjwal2000@gmail.com>
2022-10-15 10:32:32 +0530

    login page frontend

---
+++ frontend/src/Login.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div className="login-container">
+      <h1 className="login-heading">Login</h1>
+      <input className="login-input input" placeholder="Email"></input>
+      <input className="login-input input" placeholder="Password"></input>
+      <button className="btn">LogIn</button>
+      <p className="suggestion">
+        Are u a New Teacher?<a>Register as Teacher</a>
+      </p>
+    </div>
+  );
+}
+
+export default Login;
